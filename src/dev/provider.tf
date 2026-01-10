d1ff207ce75bb00e944863cf7915dd54b50d298d
Gurdip Sira <87333788+GurdipS5@users.noreply.github.com>
2026-01-10 01:41:03 +0000

    Update provider.tf

--- src/dev/provider.tf
+++ src/dev/provider.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.14"
+  
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
 provider "github" {
   token = data.vault_kv_secret_v2.github.data["token"]
   owner = data.vault_kv_secret_v2.github.data["owner"]
